fe38eed088e5ae023dd90a88605cfe55ab71d674
Lukas <huberniku@gmail.com>
2021-12-14 15:40:05 +0100

    avoider working in static scnenario

--- fast_obstacle_avoidance/obstacle_avoider.py
+++ fast_obstacle_avoidance/obstacle_avoider.py
@@ -29,7 +29,9 @@ class FastObstacleAvoider:
     def __init__(self, robot: ControlRobot):
         self.robot = robot
 
-    def evaluate(self, initial_velocity: np.ndarray, laser_scan: np.ndarray):
+    def evaluate(self,
+                 initial_velocity: np.ndarray, laser_scan: np.ndarray,
+                 limit_velocity_magnitude: bool = True):
         """ Modulate velocity and return DS. """
         # For each control_points
         # -> get distance (minus radius)
@@ -44,18 +46,41 @@ class FastObstacleAvoider:
             return initial_velocity
 
         deomposition_matrix = get_orthogonal_basis(reference_direction/ref_norm, normalize=False)
+        stretching_vector = self.get_stretching_vector(
+            ref_norm, reference_direction, initial_velocity)
         
-        weight = self.get_weight_from_norm(ref_norm)
-        stretching_vector = np.hstack((
-            1-weight,
-            1+weight * np.ones(reference_direction.shape[0]-1)
-            ))
-
         modulated_velocity = deomposition_matrix.T @ initial_velocity
         modulated_velocity = np.diag(stretching_vector) @ modulated_velocity
         modulated_velocity = deomposition_matrix @ modulated_velocity
-        
+
+        if limit_velocity_magnitude:
+            mod_norm = LA.norm(modulated_velocity)
+            init_norm = LA.norm(initial_velocity)
+            
+            if mod_norm > init_norm:
+                modulated_velocity = modulated_velocity * (init_norm/mod_norm)
+            
         return modulated_velocity
+
+    def get_stretching_vector(
+        self, ref_norm: float,
+        reference_direction: np.ndarray = None,
+        initial_velocity: np.ndarray = None,
+        tail_effect: bool = False) -> np.ndarray:
+        weight = self.get_weight_from_norm(ref_norm)
+
+        if np.dot(reference_direction, initial_velocity) < 0:
+            normal_stretch = 1
+        else:
+            
+            normal_stretch = 1 - weight
+        
+        stretching_vector = np.hstack((
+            normal_stretch,
+            1+weight * np.ones(reference_direction.shape[0]-1)
+            ))
+        
+        return stretching_vector
 
     def get_reference_direction(self, laser_scan: np.ndarray) -> np.ndarray:
         relative_position, relative_distances = (
@@ -67,20 +92,29 @@ class FastObstacleAvoider:
             relative_position * np.tile(weights, (relative_position.shape[0], 1)),
             axis=1
             )
+
+        # fig, ax = plt.subplots(figsize=(12, 6))
+        # ax.plot(laser_scan[0, :], laser_scan[1, :], '.', color='k')
+        # ax.plot(0, 0, 'o', color='r')
+
+        # ax.quiver(laser_scan[0, :], laser_scan[1, :],
+                  # relative_position[0, :]*weights,
+                  # relative_position[1, :]*weights, color='r', scale=2)
         
         return reference_direction
 
     def get_weight_from_norm(self, norm):
         return norm
     
-    def get_weight_from_distances(self, distances, weight_factor=1.0, margin_weight=1e-3):
+    def get_weight_from_distances(self, distances, weight_factor=0.1, weight_power=2.0, margin_weight=1e-3):
         # => get weighted evaluation along the robot
         # to obtain linear + angular velocity
         if any(distances < margin_weight):
             warnings.warn("Treat the small-weight case.")
 
             distances = distances - np.min(distances) + margin_weight
-        weight = 1 / distances * weight_factor
+
+        weight = (1 / distances)**weight_power * weight_factor
 
         weight_sum = np.sum(weight)
         
--- scripts/bag_evaulate.py
+++ scripts/bag_evaulate.py
@@ -8,10 +8,61 @@ import matplotlib.pyplot as plt
 import rosbag
 
 from vartools.states import ObjectPose
-from vartools.dynamical_systems import LinearSystem 
+from vartools.dynamical_systems import LinearSystem
+from vartools.animator import Animator
 
 from fast_obstacle_avoidance.control_robot import ControlRobot
 from fast_obstacle_avoidance.obstacle_avoider import FastObstacleAvoider
+
+
+class LaserScanAnimator(Animator):
+    def setup(self, static_laserscan, initial_dynamics, robot):
+        self.robot = robot
+        self.initial_dynamics = initial_dynamics
+        self.fast_avoider = FastObstacleAvoider(robot=self.robot)
+        self.static_laserscan = static_laserscan
+
+        self.fig, self.ax = plt.subplots(figsize=(12, 6))
+
+    def update_step(self, ii):
+        """ Update robot and position."""
+        initial_velocity = self.initial_dynamics.evaluate(self.robot.pose.position)
+        modulated_velocity = self.fast_avoider.evaluate(initial_velocity, self.static_laserscan)
+
+        # Update qolo
+        self.robot.pose.position = self.robot.pose.position + self.dt_simulation*modulated_velocity
+        # self.robot.pose.orientation += self.dt_simulation*modulated_velocity
+
+        self.ax.clear()
+        # Plot
+        self.ax.plot(self.robot.pose.position[0],
+                     self.robot.pose.position[1], 'o', color='k')
+        self.ax.plot(self.static_laserscan[0, :], self.static_laserscan[1, :], '.', color='k')
+        
+        self.ax.set_xlim([-10, 5])
+        self.ax.set_ylim([-5, 5])
+        self.ax.set_aspect('equal')
+
+        self.ax.arrow(self.robot.pose.position[0], self.robot.pose.position[1],
+                      initial_velocity[0], initial_velocity[1], 
+                      width=0.05, head_width=0.3,
+                      color='g',
+                      label="Initial")
+    
+        self.ax.arrow(self.robot.pose.position[0], self.robot.pose.position[1],
+                      modulated_velocity[0], modulated_velocity[1],
+                      width=0.05, head_width=0.3,
+                      color='b',
+                      label="Modulated")
+
+        self.robot.plot2D(self.ax)
+        self.ax.plot(self.initial_dynamics.attractor_position[0],
+                     self.initial_dynamics.attractor_position[1], '*', color='black') 
+        # print(tt)
+        self.ax.grid()
+        
+    # def has_converged(self):
+        # return (self.it > self.it_max)
 
 
 def laserscan_to_numpy(msg, dimension=2, delta_angle=0, delta_position=None) -> np.ndarray:
@@ -28,11 +79,13 @@ def laserscan_to_numpy(msg, dimension=2, delta_angle=0, delta_position=None) -> 
         # Rotate
         cos_val = np.cos(delta_angle)
         sin_val = np.sin(delta_angle)
+        
         rot_matr = np.array([[cos_val, sin_val],
                              [-sin_val, cos_val]])
+        
         delta_position = rot_matr @ delta_position
 
-        postitions = positions + np.tile(delta_position, (positions.shape[1], 1)).T
+        positions = positions + np.tile(delta_position, (positions.shape[1], 1)).T
     
     return positions
 
@@ -40,6 +93,45 @@ def get_topics(rosbag_name):
     import bagpy
     # get the list of topics
     print(bagpy.bagreader(rosbag_name).topic_table)
+
+
+def static_plot(allscan, qolo, dynamical_system, fast_avoider):
+    initial_velocity = dynamical_system.evaluate(qolo.pose.position)
+    modulated_velocity = fast_avoider.evaluate(initial_velocity, allscan)    
+
+    fig, ax = plt.subplots(figsize=(12, 6))
+
+    ax.plot(0, 0, 'o', color='k')
+    # ax.plot(frontscan[0, :], frontscan[1, :], '.', color='r')
+    # ax.plot(rearscan[0, :], rearscan[1, :], '.', color='b')
+    ax.plot(allscan[0, :], allscan[1, :], '.', color='k')
+
+    ax.set_xlim([-10, 5])
+    ax.set_ylim([-5, 5])
+    ax.set_aspect('equal')
+
+    ax.arrow(qolo.pose.position[0], qolo.pose.position[1],
+             initial_velocity[0], initial_velocity[1], 
+             width=0.05, head_width=0.3,
+             color='g',
+             label="Initial")
+    
+    ax.arrow(qolo.pose.position[0], qolo.pose.position[1],
+             modulated_velocity[0], modulated_velocity[1],
+             width=0.05, head_width=0.3,
+             color='b',
+             label="Modulated")
+
+    ax.legend()
+
+    qolo.plot2D(ax)
+    # print(tt)
+    ax.grid()
+    # for topic, msg, t in my_bag.read_messages(topics=[
+         # '/front_lidar/scan',
+         # '/rear_lidar/scan'
+    # ]):
+        # breakpoint()
     
 
 def main():
@@ -66,13 +158,17 @@ def main():
             frontscan = laserscan_to_numpy(msg)
             
         elif topic == '/rear_lidar/scan':
-            rearscan = laserscan_to_numpy(msg, delta_angle=np.pi, delta_position=[0.5, 0])
-
+            rearscan = laserscan_to_numpy(
+                msg, delta_angle=np.pi, delta_position=[0.75, 0.0])
             
         if frontscan is not None and rearscan is not None:
             break
 
     allscan = np.hstack((rearscan, frontscan))
+
+    # Downsample for DEBUGGING only
+    # sample_freq = 20
+    # allscan = allscan[:,  np.logical_not(np.mod(np.arange(allscan.shape[1]), sample_freq))]
 
     qolo = ControlRobot(
         control_points=np.array([[0, 0],
@@ -82,39 +178,30 @@ def main():
         control_radiuses=np.array([0.5,
                                    # 0.4,
                                    ]),
-        pose = ObjectPose(position=[0, 0], orientation=30*np.pi/180)
+        pose = ObjectPose(position=[0.7, -0.7], orientation=30*np.pi/180)
     )
-
+    
     fast_avoider = FastObstacleAvoider(robot=qolo)
 
-    dynamical_system = LinearSystem(attractor_position=np.array([2, 4]))
-    initial_velocity = dynamical_system.evaluate(qolo.pose.position)
-    modulated_velocity = fast_avoider.evaluate(initial_velocity, allscan)
+    dynamical_system = LinearSystem(
+        attractor_position=np.array([-2, 2]), maximum_velocity=0.8)
 
-    plt.ion()
-    fig, ax = plt.subplots(figsize=(12, 6))
+    # static_plot(allscan, qolo, dynamical_system, fast_avoider)
+    # breakpoint()
 
-    ax.plot(0, 0, 'o', color='k')
-    ax.plot(frontscan[0, :], frontscan[1, :], '.', color='r')
-    ax.plot(rearscan[0, :], rearscan[1, :], '.', color='b')
+    main_animator = LaserScanAnimator(it_max=200, dt_simulation=0.04)
+    main_animator.setup(
+        static_laserscan=allscan,
+        initial_dynamics=dynamical_system,
+        robot=qolo,
+        )
 
-    ax.set_xlim([-10, 5])
-    ax.set_ylim([-5, 5])
-    ax.set_aspect('equal')
-
-    qolo.plot2D(ax)
-    # print(tt)
-    ax.grid()
-    # for topic, msg, t in my_bag.read_messages(topics=[
-         # '/front_lidar/scan',
-         # '/rear_lidar/scan'
-    # ]):
-        # breakpoint()
-
-    
-    
+    main_animator.run()
+    # main_animator.update_step(ii=0)
 
 if (__name__) == "__main__":
     plt.close('all')
+    plt.ion()
+
     main()
     pass
