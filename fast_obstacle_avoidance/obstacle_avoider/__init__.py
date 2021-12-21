2f00a3ac31026a559611d63c320f8b044266bfef
Lukas <huberniku@gmail.com>
2021-12-21 13:30:58 +0100

    add file-skeleton

--- fast_obstacle_avoidance/obstacle_avoider/__init__.py
+++ fast_obstacle_avoidance/obstacle_avoider/__init__.py
@@ -5,9 +5,11 @@ The :mod:`collision_avoider` module implements various types of obstacles.
 from ._base import SingleModulationAvoider
 from .obstacle_avoider import FastObstacleAvoider
 from .lidar_avoider import FastLidarAvoider
+from .mixed_avoider import MixedEnvironmentAvoider
 
 __all__ = [
     "SingleModulationAvoider",
     "FastObstacleAvoider",
     "FastLidarAvoider"
+    "MixedEnvironmentAvoider"
     ]
---
+++ fast_obstacle_avoidance/obstacle_avoider/mixed_avoider.py
@@ -0,0 +1,13 @@
+class MixedEnvironmentAvoider(SingleModulationAvoider):
+    def __init__(self):
+        pass
+
+    def update_obstacles(self):
+        pass
+    
+    def update_laserscan(self):
+        pass
+
+    def update_reference_direction(self):
+        pass
+        
