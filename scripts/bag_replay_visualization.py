10a1a665aaaba502cad27cc451aef1a9b2c49362
Lukas <huberniku@gmail.com>
2022-01-28 20:43:14 +0100

    batch player working

--- scripts/bag_replay_visualization.py
+++ scripts/bag_replay_visualization.py
@@ -5,8 +5,10 @@
 
 import sys
 import os
+
 import datetime
 import subprocess
+
 from timeit import default_timer as timer
 
 import math
@@ -102,8 +104,9 @@ class ReplayQoloCording(Animator):
         self,
         robot,
         my_bag,
-        x_lim=[-7.5, 0],
-        y_lim=[-4.0, 4.0],
+        x_lim=None,
+        y_lim=None,
+        plot_width_x = 11, plot_width_y = 8,
         position_storing_length=50,
     ):
         self.robot = robot
@@ -112,11 +115,15 @@ class ReplayQoloCording(Animator):
 
         self.static_laserscan = None
 
-        self.fig, self.ax = plt.subplots(figsize=(12, 8))
+        self.fig, self.ax = plt.subplots(figsize=(16, 10))
         self.obstacle_color = np.array([177, 124, 124]) / 255.0
 
         self.x_lim = x_lim
         self.y_lim = y_lim
+
+        # Only used if no x_lim / y_lim given
+        self.width_x = plot_width_x
+        self.width_y = plot_width_y
 
         self.dimension = 2
         # self.position_list = np.zeros((self.dimension, position_storing_length))
@@ -132,6 +139,7 @@ class ReplayQoloCording(Animator):
         self.position_list = np.tile(
             self.robot.pose.position, (position_storing_length, 1)
         ).T
+        
 
     def update_step(self, ii):
         self.ii = ii
@@ -166,15 +174,10 @@ class ReplayQoloCording(Animator):
             c=np.arange(self.position_list.shape[1]),
             marker=".",
             s=30,
+            alpha=0.6,
         )
 
-        self.ax.text(
-            self.x_lim[1] - 0.1 * (self.x_lim[1] - self.x_lim[0]),
-            self.y_lim[1] - 0.05 * (self.y_lim[1] - self.y_lim[0]),
-            f"{str(round(self.simulation_time, 2))} s",
-            fontsize=14,
-        )
-
+        
         laserscan = self.robot.get_allscan(in_robot_frame=False)
 
         if laserscan.shape[1]:
@@ -195,8 +198,43 @@ class ReplayQoloCording(Animator):
                 zorder=-1,
             )
 
+        if self.x_lim is None or self.y_lim is None:
+            # Just define both if one is not defined
+            sensor_center = np.mean(laserscan, axis=1)
+
+            self.x_lim = [sensor_center[0] - self.width_x/2,
+                          sensor_center[0] + self.width_x/2]
+
+            self.y_lim = [sensor_center[1] - self.width_y/2,
+                          sensor_center[1] + self.width_y/2]
+        
+
+        # Check offset and potentially adapt
+        fraction_to_adapt_plot_limits = 1./3
+        if (abs(np.mean(self.x_lim) - self.robot.pose.position[0])
+            > fraction_to_adapt_plot_limits * (self.x_lim[1]-self.x_lim[0])):
+            sensor_center = np.mean(laserscan, axis=1)
+
+            self.x_lim = [sensor_center[0] - self.width_x/2,
+                          sensor_center[0] + self.width_x/2]
+
+        if (abs(np.mean(self.y_lim) - self.robot.pose.position[1])
+            > fraction_to_adapt_plot_limits * (self.y_lim[1]-self.y_lim[0])):
+            sensor_center = np.mean(laserscan, axis=1)
+
+            self.y_lim = [sensor_center[1] - self.width_y/2,
+                          sensor_center[1] + self.width_y/2]
+
         self.ax.set_xlim(self.x_lim)
         self.ax.set_ylim(self.y_lim)
+
+        self.ax.text(
+            self.x_lim[1] - 0.1 * (self.x_lim[1] - self.x_lim[0]),
+            self.y_lim[1] - 0.05 * (self.y_lim[1] - self.y_lim[0]),
+            f"{str(round(self.simulation_time, 2))} s",
+            fontsize=14,
+        )
+
         self.ax.set_aspect("equal")
         self.ax.grid(zorder=-3.0)
 
@@ -243,35 +281,52 @@ class ReplayQoloCording(Animator):
         pass
 
 
-def evaluate_run_in_room(
+def evaluate_bag(
     my_bag,
-    x_lim,
-    y_lim,
+    x_lim=None,
+    y_lim=None,
     t_max=10,
     dt_simulation=0.1,
     animation_name=None,
     save_animation=False,
     bag_name=None,
     bag_dir=None,
+    plot_width_x=None,
+    plot_width_y=None,
 ):
     bag_path = bag_dir + bag_name
-    # result = subprocess.run([f'rosbag info {bag_path} | grep duration'], stdout=subprocess.PIPE)
-    # result = subprocess.run([f'rosbag info {bag_path}'], stdout=subprocess.PIPE)
-    result = subprocess.run([f"rosbag info {bag_path}"], stdout=subprocess.PIPE)
-    # path = subprocess.run([f'pwd'], stdout=subprocess.PIPE)
 
-    breakpoint()
+    # Get duration
+    stream = os.popen(f'rosbag info {bag_path} | grep duration')
+    output = stream.read()
+    
+    duration = 0
+    duration_str = ""
+    for m in output:
+        if len(duration_str) > 0 or m.isdigit():
+            if m=="s":
+                break
+            
+            elif m == ":":
+                # Minute / hour
+                duration = 60*float(duration_str)
+                continue
+                
+            duration_str = duration_str + m
+    duration = duration + float(duration_str)
+
+    it_max = int(duration / dt_simulation)
+    
     qolo = qolo = QoloRobot(
         pose=ObjectPose(position=np.array([0, 0]), orientation=0 * np.pi / 180)
     )
 
-    if animation_name is not None:
-        now = datetime.datetime.now()
-        animation_name = f"{animation_name}_{now:%Y-%m-%d_%H-%M-%S}"
+    if animation_name is None:
+        animation_name = f"animation_bag_{bag_name[:-4]}"
 
     replayer = ReplayQoloCording(
-        it_max=int(t_max / dt_simulation),
-        dt_simulation=0.1,
+        it_max=it_max,
+        dt_simulation=dt_simulation,
         animation_name=animation_name,
     )
 
@@ -280,6 +335,8 @@ def evaluate_run_in_room(
         my_bag=my_bag,
         x_lim=x_lim,
         y_lim=y_lim,
+        plot_width_x=plot_width_x,
+        plot_width_y=plot_width_y,
     )
 
     replayer.run(save_animation=save_animation)
@@ -289,11 +346,11 @@ def evaluate_run_in_room(
 first_simulation_options = {
     "bag_dir": "../data_qolo/indoor_working/",
     "bag_name": "2022-01-24-18-33-30.bag",
-    "x_lim": [-9.5, 1.5],
-    "y_lim": [-4.0, 4.0],
-    "t_max": 58,
+    # "x_lim": [-9.5, 1.5],
+    # "y_lim": [-4.0, 4.0],
+    # "t_max": 58,
     "dt_simulation": 0.1,
-    "animation_name": "animation_first_indoor",
+    # "animation_name": "animation_first_indoor",
 }
 
 second_simulation_options = {
@@ -306,34 +363,66 @@ second_simulation_options = {
     "animation_name": "animation_second_indoor",
 }
 
-third_simulation_options = {
+Third_simulation_options = {
     "bag_dir": "../data_qolo/indoor_working/",
     "bag_name": "2022-01-26-18-21-31.bag",
-    "x_lim": [-9.0, 3.0],
-    "y_lim": [-3.0, 5.0],
-    "t_max": 100,
+    # "x_lim": [-9.0, 3.0],
+    # "y_lim": [-3.0, 5.0],
+    # "t_max": 100,
     "dt_simulation": 0.1,
-    "animation_name": "animation_upperbody_first",
+    # "animation_name": "animation_upperbody_first",
 }
+
+def evaluate_multibags(
+    bag_dir="../data_qolo/marketplace_lausanne_2022_01_28/",
+    save_animation=True,
+    dt_evaluation=0.1,
+    # plot_width_x=11, plot_width_y=8,
+    # plot_width_x=18, plot_width_y=12,
+    plot_width_x=16, plot_width_y=9,
+    # 
+    ):
+    """ This script is for batch-processing of the ros-bag recording with the qolo."""
+    # Use aspect ratio of 16:9 [youtube standard ration] for x_lim / y_lim
+    # bag_list = glob.glob(bag_dir + "/*.bag")
+    bag_list = os.listdir(bag_dir)
+
+    for bag_name in bag_list:
+        my_bag = rosbag.Bag(bag_dir + bag_name)
+
+        evaluate_bag(
+            my_bag,
+            bag_name=bag_name,
+            bag_dir=bag_dir,
+            save_animation=save_animation,
+            dt_simulation=dt_evaluation,
+            plot_width_x=plot_width_x,
+            plot_width_y=plot_width_y,
+        )
+
 
 
 if (__name__) == "__main__":
     plt.close("all")
     plt.ion()
 
-    save_animation = False
-    # my_simu = first_simulation_options
-    # my_simu = second_simulation_options
-    my_simu = third_simulation_options
+    evaluate_multibags()
 
-    reimport_bag = False
-    if (
-        reimport_bag
-        or not "my_bag" in locals()
-        or not my_bag_name == my_simu["bag_name"]
-    ):
+    evaluate_single_bag = False
+    if evaluate_single_bag:
+        save_animation = False
+        my_simu = first_simulation_options
+        # my_simu = second_simulation_options
+        # my_simu = third_simulation_options
 
-        my_bag = rosbag.Bag(my_simu["bag_dir"] + my_simu["bag_name"])
-        my_bag_name = my_simu["bag_name"]
+        reimport_bag = False
+        if (
+            reimport_bag
+            or not "my_bag" in locals()
+            or not my_bag_name == my_simu["bag_name"]
+        ):
 
-    evaluate_run_in_room(my_bag, save_animation=save_animation, **my_simu)
+            my_bag = rosbag.Bag(my_simu["bag_dir"] + my_simu["bag_name"])
+            my_bag_name = my_simu["bag_name"]
+
+        evaluate_run_in_room(my_bag, save_animation=save_animation, **my_simu)
