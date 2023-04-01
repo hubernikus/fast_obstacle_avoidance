9789f3a121534d25baa994de142f1c8de4e008ce
Lukas Huber <hubernikus@gmail.com>
2023-04-01 19:04:57 +0200

    Remove rendundant information.

--- examples/example_cluster_sample.py
+++ examples/example_cluster_sample.py
@@ -3,34 +3,20 @@
 # Created: 2023-04-01
 # Email: lukas.huber@epfl.ch
 
-import copy
-
-from timeit import default_timer as timer
-
 import numpy as np
-from numpy import linalg as LA
-
 import matplotlib.pyplot as plt
-
-import shapely
 
 from vartools.states import ObjectPose
 from vartools.animator import Animator
-from vartools.dynamical_systems import ConstantValue, LinearSystem
-from vartools.dynamical_systems import plot_dynamical_system_streamplot
-from vartools.dynamical_systems import plot_dynamical_system_quiver
+from vartools.dynamical_systems import LinearSystem
 
+from fast_obstacle_avoidance.sampling_container import SampledCuboid
 from fast_obstacle_avoidance.obstacle_avoider import SampledClusterAvoider
 from fast_obstacle_avoidance.control_robot import QoloRobot
 
-from fast_obstacle_avoidance.sampling_container import visualize_obstacles
 from fast_obstacle_avoidance.sampling_container import ShapelySamplingContainer
-from fast_obstacle_avoidance.sampling_container import SampledEllipse, SampledCuboid
 
 from fast_obstacle_avoidance.visualization import LaserscanAnimator
-from fast_obstacle_avoidance.visualization import (
-    static_visualization_of_sample_avoidance,
-)
 
 
 def get_two_obstacle_sampler():
@@ -53,59 +39,6 @@ def get_two_obstacle_sampler():
         )
     )
     return main_environment
-
-
-def execute_avoidance_with_two_obstacles(
-    save_figure=False,
-    create_animation=False,
-    algorithmtype: AlgorithmType = AlgorithmType.SAMPLED,
-    it_max: int = 170,
-):
-    """Visualizes
-    (1) the repulsion from a specific point
-    (2) the resulting vector-field."""
-
-    # dynamical_system = ConstantValue(velocity=[0, 1])
-    initial_dynamics = LinearSystem(
-        attractor_position=np.array([3.5, 1.3]), maximum_velocity=1.0
-    )
-
-    environment_sampler = get_two_obstacle_sampler()
-
-    start_point = np.array([-2.5, 1])
-    robot = QoloRobot(pose=ObjectPose(position=start_point, orientation=0))
-    robot.control_point = [0, 0]
-    robot.control_radius = 0.4
-    # robot.pose.position = start_point
-
-    fast_avoider = SampledClusterAvoider(robot=robot)
-    fast_avoider.weight_factor = 2 * np.pi / environment_sampler.n_samples * 1
-    fast_avoider.weight_power = 1.0 / 2
-    fast_avoider.weight_max_norm = 1e7
-
-    my_animator = LaserscanAnimator(
-        it_max=it_max,
-        dt_simulation=0.05,
-        # dt_pause=0.1,
-        file_type=".gif",
-        animation_name="two_obstacle_avoidance" + "_" + "clustersampled",
-    )
-
-    my_animator.setup(
-        robot=robot,
-        initial_dynamics=initial_dynamics,
-        avoider=fast_avoider,
-        environment=environment_sampler,
-        x_lim=[-4, 4.5],
-        y_lim=[-1.0, 5.6],
-        plot_lidarlines=True,
-        show_reference=True,
-        show_lidarweight=False,
-    )
-
-    my_animator.run(save_animation=save_figure)
-
-    print(f"Done the animation. Saved={save_figure}.")
 
 
 class GapPassingAnimator(Animator):
